ee13f2e67ac8816961c0bc12015a880b3129faf6
nawab <nawab@hamilton.(none)>
2012-04-30 17:15:19 -0700

    f

--- src/apps/baseline.java
+++ src/apps/baseline.java
@@ -21,6 +21,9 @@ import org.apache.zookeeper.data.Stat;
 public class baseline implements Watcher {
 
 	static ZooKeeper zk;
+	static int counter = 1000;
+	
+	static long starttime;
 	
 	/**
 	 * This function is notified when an event we watched
@@ -28,7 +31,36 @@ public class baseline implements Watcher {
 	 */
 	@Override
 	public void process(WatchedEvent event) {
-		System.out.println ("I watched!");
+		//System.out.println ("I watched! "+counter);
+		counter--;
+		if (counter == 0){
+			try {
+				zk.close();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println ( System.currentTimeMillis() - starttime );
+			System.exit(0);
+		}
+		
+		
+		Stat s;
+		try {
+			s = zk.exists("/faisal2", true);
+			if ( s == null ) {
+				zk.create("/faisal2", new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+			}
+			zk.delete("/faisal2", -1);
+			zk.
+		} catch (KeeperException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 	
 	baseline () {
@@ -43,16 +75,19 @@ public class baseline implements Watcher {
 	
 	public static void main (String argv[]) {
 		
+		
 		baseline mt = new baseline ();
 		
-		
+		starttime = System.currentTimeMillis();
 		try {
-			Stat s = zk.exists("/faisal2", true);
-			if ( s == null ) {
-				zk.create("/faisal2", new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-				zk.delete("/faisal2", -1);
-			}
+//			try {
+//				zk.delete("/faisal2", -1)
+//			} catch (Exception e) {
+//				// ignore
+//			}
 			
+			zk.create("/faisal2", new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
 		} catch (KeeperException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
