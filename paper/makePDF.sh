dcf686e2f9825603871f7d20be21bf394bf093e1
nawab <nawab@hamilton.(none)>
2012-05-17 19:59:19 -0700

    paper first

---
+++ paper/makePDF.sh
@@ -0,0 +1,2 @@
+latex $1.tex ; bibtex $1 ; latex $1.tex ; latex $1.tex ; dvipdf $1.dvi
+#latex TMAC-camera-ready-8May.tex; dvips -o TMAC-camera-ready-8May.ps TMAC-camera-ready-8May.dvi; ps2pdf TMAC-camera-ready-8May.ps
