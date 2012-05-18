d6f93da7cc444c1e74b439c44dcc24e5bb7b9a7f
nawab <nawab@hamilton.(none)>
2012-05-17 20:31:46 -0700

    makepdf

--- paper/makePDF.sh
+++ paper/makePDF.sh
@@ -1,2 +1,8 @@
+if [ $# -ne 1 ]
+then
+	echo "enter the name of the primary tex file. if it is xxxx.tex, then enter $1 xxxx without the .tex"
+	exit
+fi
+
 latex $1.tex ; bibtex $1 ; latex $1.tex ; latex $1.tex ; dvipdf $1.dvi
 #latex TMAC-camera-ready-8May.tex; dvips -o TMAC-camera-ready-8May.ps TMAC-camera-ready-8May.dvi; ps2pdf TMAC-camera-ready-8May.ps
