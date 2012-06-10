ac3e0c4f6ef12d4a4d78a5673b0ef9704d79bc7f
Alexander <pucher@cs.ucsb.edu>
2012-06-09 17:47:56 -0700

    replace run script
    Signed-off-by: Alexander <pucher@cs.ucsb.edu>

--- zkbm/scripts/run_multiTest.sh
+++ zkbm/scripts/run_multiTest.sh
@@ -1,3 +1,2 @@
-ZKBM_DIR=/home/nawab/workspace/zkbm/
-java -cp ${ZKBM_DIR}/zkbm/lib/commons-cli-1.2.jar:$ZKBM_DIR/zkbm/lib/commons-math3-3.0.jar:$ZKBM_DIR/zkbm/lib/zookeeper-3.3.5.jar:$ZKBM_DIR/zkbm/lib/log4j-1.2.15.jar:$ZKBM_DIR/zkbm.jar Test.testMulti $*
+java -cp ../target/zkbm-1.0-SNAPSHOT.jar edu.ucsb.cs.zkbm.testMulti $*
 
