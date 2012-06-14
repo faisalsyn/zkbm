a984ad6ddd0ceb86e3473b331600a654df1677b3
Alexander Pucher <dev@alexpucher.com>
2012-06-14 15:50:33 -0700

    extend test framework
    Signed-off-by: Alexander Pucher <dev@alexpucher.com>

--- zkbm/src/main/java/edu/ucsb/cs/zkbm/testAsyncQueueEx.java
+++ zkbm/src/main/java/edu/ucsb/cs/zkbm/testAsyncQueueEx.java
@@ -1,13 +1,13 @@
 package edu.ucsb.cs.zkbm;
 
-public class testQueue {
+public class testAsyncQueueEx {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-			ZkQueue zktas = new ZkQueue("aa", "rubicon.cs.ucsb.edu");
+			ZkAsyncQueueEx zktas = new ZkAsyncQueueEx("aa", "rubicon.cs.ucsb.edu");
 
 			long startt = System.currentTimeMillis();
 			while (!zktas.acquire())
--- zkbm/src/main/java/edu/ucsb/cs/zkbm/testTestAndSet.java
+++ zkbm/src/main/java/edu/ucsb/cs/zkbm/testTestAndSet.java
@@ -6,27 +6,17 @@ public class testTestAndSet {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		for (int i = 0 ; i < 10 ; i++ ) {
-
-			ZkAsyncTestAndSet zktas = new ZkAsyncTestAndSet ("aa", "rubicon.cs.ucsb.edu");
+		for (int i = 0; i < 10; i++) {
+			ZkTestAndSet zktas = new ZkTestAndSet("aa", "rubicon.cs.ucsb.edu");
 
 			long startt = System.currentTimeMillis();
-			
-			boolean state;
-			
-			state = zktas.acquire();
-			
-			System.out.println( state) ;
-
+			while (!zktas.acquire())
+				;
 			zktas.release();
-			System.out.println ((System.currentTimeMillis() - startt));
-			
+			System.out.println((System.currentTimeMillis() - startt));
+
 			zktas.close();
 		}
-
-//		System.in.read(arg0);
 	}
 
 }
